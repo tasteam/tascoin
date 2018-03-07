4db736f684c435e7af2a3f8a4b0e9c81d6307325
root <root@instance-1.c.strange-mind-194405.internal>
2018-03-07 16:35:02 +0000

    tascoin

---
+++ build/release/src/CMakeFiles/Wallet.dir/DependInfo.cmake
@@ -0,0 +1,46 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_CXX
+  "/root/tascoin/src/Wallet/LegacyKeysImporter.cpp" "/root/tascoin/build/release/src/CMakeFiles/Wallet.dir/Wallet/LegacyKeysImporter.cpp.o"
+  "/root/tascoin/src/Wallet/WalletAsyncContextCounter.cpp" "/root/tascoin/build/release/src/CMakeFiles/Wallet.dir/Wallet/WalletAsyncContextCounter.cpp.o"
+  "/root/tascoin/src/Wallet/WalletErrors.cpp" "/root/tascoin/build/release/src/CMakeFiles/Wallet.dir/Wallet/WalletErrors.cpp.o"
+  "/root/tascoin/src/Wallet/WalletGreen.cpp" "/root/tascoin/build/release/src/CMakeFiles/Wallet.dir/Wallet/WalletGreen.cpp.o"
+  "/root/tascoin/src/Wallet/WalletRpcServer.cpp" "/root/tascoin/build/release/src/CMakeFiles/Wallet.dir/Wallet/WalletRpcServer.cpp.o"
+  "/root/tascoin/src/Wallet/WalletSerializationV1.cpp" "/root/tascoin/build/release/src/CMakeFiles/Wallet.dir/Wallet/WalletSerializationV1.cpp.o"
+  "/root/tascoin/src/Wallet/WalletSerializationV2.cpp" "/root/tascoin/build/release/src/CMakeFiles/Wallet.dir/Wallet/WalletSerializationV2.cpp.o"
+  "/root/tascoin/src/Wallet/WalletUtils.cpp" "/root/tascoin/build/release/src/CMakeFiles/Wallet.dir/Wallet/WalletUtils.cpp.o"
+  "/root/tascoin/src/WalletLegacy/KeysStorage.cpp" "/root/tascoin/build/release/src/CMakeFiles/Wallet.dir/WalletLegacy/KeysStorage.cpp.o"
+  "/root/tascoin/src/WalletLegacy/WalletHelper.cpp" "/root/tascoin/build/release/src/CMakeFiles/Wallet.dir/WalletLegacy/WalletHelper.cpp.o"
+  "/root/tascoin/src/WalletLegacy/WalletLegacy.cpp" "/root/tascoin/build/release/src/CMakeFiles/Wallet.dir/WalletLegacy/WalletLegacy.cpp.o"
+  "/root/tascoin/src/WalletLegacy/WalletLegacySerialization.cpp" "/root/tascoin/build/release/src/CMakeFiles/Wallet.dir/WalletLegacy/WalletLegacySerialization.cpp.o"
+  "/root/tascoin/src/WalletLegacy/WalletLegacySerializer.cpp" "/root/tascoin/build/release/src/CMakeFiles/Wallet.dir/WalletLegacy/WalletLegacySerializer.cpp.o"
+  "/root/tascoin/src/WalletLegacy/WalletTransactionSender.cpp" "/root/tascoin/build/release/src/CMakeFiles/Wallet.dir/WalletLegacy/WalletTransactionSender.cpp.o"
+  "/root/tascoin/src/WalletLegacy/WalletUnconfirmedTransactions.cpp" "/root/tascoin/build/release/src/CMakeFiles/Wallet.dir/WalletLegacy/WalletUnconfirmedTransactions.cpp.o"
+  "/root/tascoin/src/WalletLegacy/WalletUserTransactionsCache.cpp" "/root/tascoin/build/release/src/CMakeFiles/Wallet.dir/WalletLegacy/WalletUserTransactionsCache.cpp.o"
+  )
+set(CMAKE_CXX_COMPILER_ID "GNU")
+
+# Preprocessor definitions for this target.
+set(CMAKE_TARGET_DEFINITIONS_CXX
+  "STATICLIB"
+  )
+
+# The include file search paths:
+set(CMAKE_CXX_TARGET_INCLUDE_PATH
+  "../../include"
+  "../../src"
+  "../../external"
+  "version"
+  "../../src/Platform/Linux"
+  "../../src/Platform/Posix"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ build/release/tests/CMakeFiles/UnitTests.dir/DependInfo.cmake
@@ -0,0 +1,97 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_CXX
+  "/root/tascoin/tests/UnitTests/ArrayRefTests.cpp" "/root/tascoin/build/release/tests/CMakeFiles/UnitTests.dir/UnitTests/ArrayRefTests.cpp.o"
+  "/root/tascoin/tests/UnitTests/ArrayViewTests.cpp" "/root/tascoin/build/release/tests/CMakeFiles/UnitTests.dir/UnitTests/ArrayViewTests.cpp.o"
+  "/root/tascoin/tests/UnitTests/Base58.cpp" "/root/tascoin/build/release/tests/CMakeFiles/UnitTests.dir/UnitTests/Base58.cpp.o"
+  "/root/tascoin/tests/UnitTests/BinarySerializationCompatibility.cpp" "/root/tascoin/build/release/tests/CMakeFiles/UnitTests.dir/UnitTests/BinarySerializationCompatibility.cpp.o"
+  "/root/tascoin/tests/UnitTests/BlockReward.cpp" "/root/tascoin/build/release/tests/CMakeFiles/UnitTests.dir/UnitTests/BlockReward.cpp.o"
+  "/root/tascoin/tests/UnitTests/BlockingQueue.cpp" "/root/tascoin/build/release/tests/CMakeFiles/UnitTests.dir/UnitTests/BlockingQueue.cpp.o"
+  "/root/tascoin/tests/UnitTests/Chacha8.cpp" "/root/tascoin/build/release/tests/CMakeFiles/UnitTests.dir/UnitTests/Chacha8.cpp.o"
+  "/root/tascoin/tests/UnitTests/Checkpoints.cpp" "/root/tascoin/build/release/tests/CMakeFiles/UnitTests.dir/UnitTests/Checkpoints.cpp.o"
+  "/root/tascoin/tests/UnitTests/DecomposeAmountIntoDigits.cpp" "/root/tascoin/build/release/tests/CMakeFiles/UnitTests.dir/UnitTests/DecomposeAmountIntoDigits.cpp.o"
+  "/root/tascoin/tests/UnitTests/EventWaiter.cpp" "/root/tascoin/build/release/tests/CMakeFiles/UnitTests.dir/UnitTests/EventWaiter.cpp.o"
+  "/root/tascoin/tests/UnitTests/ICoreStub.cpp" "/root/tascoin/build/release/tests/CMakeFiles/UnitTests.dir/UnitTests/ICoreStub.cpp.o"
+  "/root/tascoin/tests/UnitTests/ICryptoNoteProtocolQueryStub.cpp" "/root/tascoin/build/release/tests/CMakeFiles/UnitTests.dir/UnitTests/ICryptoNoteProtocolQueryStub.cpp.o"
+  "/root/tascoin/tests/UnitTests/INodeStubs.cpp" "/root/tascoin/build/release/tests/CMakeFiles/UnitTests.dir/UnitTests/INodeStubs.cpp.o"
+  "/root/tascoin/tests/UnitTests/MulDiv.cpp" "/root/tascoin/build/release/tests/CMakeFiles/UnitTests.dir/UnitTests/MulDiv.cpp.o"
+  "/root/tascoin/tests/UnitTests/ParseAmount.cpp" "/root/tascoin/build/release/tests/CMakeFiles/UnitTests.dir/UnitTests/ParseAmount.cpp.o"
+  "/root/tascoin/tests/UnitTests/PaymentGateTests.cpp" "/root/tascoin/build/release/tests/CMakeFiles/UnitTests.dir/UnitTests/PaymentGateTests.cpp.o"
+  "/root/tascoin/tests/UnitTests/Serialization.cpp" "/root/tascoin/build/release/tests/CMakeFiles/UnitTests.dir/UnitTests/Serialization.cpp.o"
+  "/root/tascoin/tests/UnitTests/SerializationKV.cpp" "/root/tascoin/build/release/tests/CMakeFiles/UnitTests.dir/UnitTests/SerializationKV.cpp.o"
+  "/root/tascoin/tests/UnitTests/Shuffle.cpp" "/root/tascoin/build/release/tests/CMakeFiles/UnitTests.dir/UnitTests/Shuffle.cpp.o"
+  "/root/tascoin/tests/UnitTests/StringBufferTests.cpp" "/root/tascoin/build/release/tests/CMakeFiles/UnitTests.dir/UnitTests/StringBufferTests.cpp.o"
+  "/root/tascoin/tests/UnitTests/StringViewTests.cpp" "/root/tascoin/build/release/tests/CMakeFiles/UnitTests.dir/UnitTests/StringViewTests.cpp.o"
+  "/root/tascoin/tests/UnitTests/TestBcS.cpp" "/root/tascoin/build/release/tests/CMakeFiles/UnitTests.dir/UnitTests/TestBcS.cpp.o"
+  "/root/tascoin/tests/UnitTests/TestBlockchainExplorer.cpp" "/root/tascoin/build/release/tests/CMakeFiles/UnitTests.dir/UnitTests/TestBlockchainExplorer.cpp.o"
+  "/root/tascoin/tests/UnitTests/TestBlockchainGenerator.cpp" "/root/tascoin/build/release/tests/CMakeFiles/UnitTests.dir/UnitTests/TestBlockchainGenerator.cpp.o"
+  "/root/tascoin/tests/UnitTests/TestCurrency.cpp" "/root/tascoin/build/release/tests/CMakeFiles/UnitTests.dir/UnitTests/TestCurrency.cpp.o"
+  "/root/tascoin/tests/UnitTests/TestFileMappedVector.cpp" "/root/tascoin/build/release/tests/CMakeFiles/UnitTests.dir/UnitTests/TestFileMappedVector.cpp.o"
+  "/root/tascoin/tests/UnitTests/TestFormatUtils.cpp" "/root/tascoin/build/release/tests/CMakeFiles/UnitTests.dir/UnitTests/TestFormatUtils.cpp.o"
+  "/root/tascoin/tests/UnitTests/TestInprocessNode.cpp" "/root/tascoin/build/release/tests/CMakeFiles/UnitTests.dir/UnitTests/TestInprocessNode.cpp.o"
+  "/root/tascoin/tests/UnitTests/TestJsonValue.cpp" "/root/tascoin/build/release/tests/CMakeFiles/UnitTests.dir/UnitTests/TestJsonValue.cpp.o"
+  "/root/tascoin/tests/UnitTests/TestMessageQueue.cpp" "/root/tascoin/build/release/tests/CMakeFiles/UnitTests.dir/UnitTests/TestMessageQueue.cpp.o"
+  "/root/tascoin/tests/UnitTests/TestPath.cpp" "/root/tascoin/build/release/tests/CMakeFiles/UnitTests.dir/UnitTests/TestPath.cpp.o"
+  "/root/tascoin/tests/UnitTests/TestPeerlist.cpp" "/root/tascoin/build/release/tests/CMakeFiles/UnitTests.dir/UnitTests/TestPeerlist.cpp.o"
+  "/root/tascoin/tests/UnitTests/TestProtocolPack.cpp" "/root/tascoin/build/release/tests/CMakeFiles/UnitTests.dir/UnitTests/TestProtocolPack.cpp.o"
+  "/root/tascoin/tests/UnitTests/TestTransactionPoolDetach.cpp" "/root/tascoin/build/release/tests/CMakeFiles/UnitTests.dir/UnitTests/TestTransactionPoolDetach.cpp.o"
+  "/root/tascoin/tests/UnitTests/TestTransfers.cpp" "/root/tascoin/build/release/tests/CMakeFiles/UnitTests.dir/UnitTests/TestTransfers.cpp.o"
+  "/root/tascoin/tests/UnitTests/TestTransfersConsumer.cpp" "/root/tascoin/build/release/tests/CMakeFiles/UnitTests.dir/UnitTests/TestTransfersConsumer.cpp.o"
+  "/root/tascoin/tests/UnitTests/TestTransfersContainer.cpp" "/root/tascoin/build/release/tests/CMakeFiles/UnitTests.dir/UnitTests/TestTransfersContainer.cpp.o"
+  "/root/tascoin/tests/UnitTests/TestTransfersContainerKeyImage.cpp" "/root/tascoin/build/release/tests/CMakeFiles/UnitTests.dir/UnitTests/TestTransfersContainerKeyImage.cpp.o"
+  "/root/tascoin/tests/UnitTests/TestTransfersSubscription.cpp" "/root/tascoin/build/release/tests/CMakeFiles/UnitTests.dir/UnitTests/TestTransfersSubscription.cpp.o"
+  "/root/tascoin/tests/UnitTests/TestUpgradeDetector.cpp" "/root/tascoin/build/release/tests/CMakeFiles/UnitTests.dir/UnitTests/TestUpgradeDetector.cpp.o"
+  "/root/tascoin/tests/UnitTests/TestWallet.cpp" "/root/tascoin/build/release/tests/CMakeFiles/UnitTests.dir/UnitTests/TestWallet.cpp.o"
+  "/root/tascoin/tests/UnitTests/TestWalletLegacy.cpp" "/root/tascoin/build/release/tests/CMakeFiles/UnitTests.dir/UnitTests/TestWalletLegacy.cpp.o"
+  "/root/tascoin/tests/UnitTests/TestWalletService.cpp" "/root/tascoin/build/release/tests/CMakeFiles/UnitTests.dir/UnitTests/TestWalletService.cpp.o"
+  "/root/tascoin/tests/UnitTests/TransactionApi.cpp" "/root/tascoin/build/release/tests/CMakeFiles/UnitTests.dir/UnitTests/TransactionApi.cpp.o"
+  "/root/tascoin/tests/UnitTests/TransactionApiHelpers.cpp" "/root/tascoin/build/release/tests/CMakeFiles/UnitTests.dir/UnitTests/TransactionApiHelpers.cpp.o"
+  "/root/tascoin/tests/UnitTests/TransactionPool.cpp" "/root/tascoin/build/release/tests/CMakeFiles/UnitTests.dir/UnitTests/TransactionPool.cpp.o"
+  "/root/tascoin/tests/UnitTests/main.cpp" "/root/tascoin/build/release/tests/CMakeFiles/UnitTests.dir/UnitTests/main.cpp.o"
+  )
+set(CMAKE_CXX_COMPILER_ID "GNU")
+
+# Preprocessor definitions for this target.
+set(CMAKE_TARGET_DEFINITIONS_CXX
+  "STATICLIB"
+  )
+
+# The include file search paths:
+set(CMAKE_CXX_TARGET_INCLUDE_PATH
+  "../../include"
+  "../../src"
+  "../../external"
+  "version"
+  "../../src/Platform/Linux"
+  "../../src/Platform/Posix"
+  "../../external/gtest/include"
+  "../../external/gtest"
+  "../../tests/../version"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  "/root/tascoin/build/release/external/gtest/CMakeFiles/gtest_main.dir/DependInfo.cmake"
+  "/root/tascoin/build/release/src/CMakeFiles/PaymentGate.dir/DependInfo.cmake"
+  "/root/tascoin/build/release/src/CMakeFiles/Wallet.dir/DependInfo.cmake"
+  "/root/tascoin/build/release/tests/CMakeFiles/TestGenerator.dir/DependInfo.cmake"
+  "/root/tascoin/build/release/src/CMakeFiles/InProcessNode.dir/DependInfo.cmake"
+  "/root/tascoin/build/release/src/CMakeFiles/NodeRpcProxy.dir/DependInfo.cmake"
+  "/root/tascoin/build/release/src/CMakeFiles/Rpc.dir/DependInfo.cmake"
+  "/root/tascoin/build/release/src/CMakeFiles/Http.dir/DependInfo.cmake"
+  "/root/tascoin/build/release/src/CMakeFiles/Transfers.dir/DependInfo.cmake"
+  "/root/tascoin/build/release/src/CMakeFiles/Serialization.dir/DependInfo.cmake"
+  "/root/tascoin/build/release/src/CMakeFiles/System.dir/DependInfo.cmake"
+  "/root/tascoin/build/release/src/CMakeFiles/Logging.dir/DependInfo.cmake"
+  "/root/tascoin/build/release/src/CMakeFiles/BlockchainExplorer.dir/DependInfo.cmake"
+  "/root/tascoin/build/release/src/CMakeFiles/CryptoNoteCore.dir/DependInfo.cmake"
+  "/root/tascoin/build/release/src/CMakeFiles/Common.dir/DependInfo.cmake"
+  "/root/tascoin/build/release/src/CMakeFiles/Crypto.dir/DependInfo.cmake"
+  "/root/tascoin/build/release/external/gtest/CMakeFiles/gtest.dir/DependInfo.cmake"
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
