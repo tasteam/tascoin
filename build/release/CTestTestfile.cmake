1d0328f42b1c9b9f042cf8669f78ecd251ed6b7a
tasteam <36435717+tasteam@users.noreply.github.com>
2018-03-27 21:15:21 +0530

    Final

--- build/release/CTestTestfile.cmake
+++
@@ -1,9 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /root/tascoin
-# Build directory: /root/tascoin/build/release
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-subdirs(external)
-subdirs(src)
-subdirs(tests)
